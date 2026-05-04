a0a2445c8b0ffdab62e147351d80eaf2a6713d5d
Gabriel Zaiac <gz.gabrielzaiac@gmail.com>
2026-05-04 10:59:22 -0300

    feat(recomandation snapshoot, IO): better integration and use of recomendations snapshoots; refactor some parts; attach IO funcs on main; delete userIO

--- src/main/java/faculdade/Books/Book.java
+++ src/main/java/faculdade/Books/Book.java
@@ -41,3 +41,4 @@ public class Book {
         return Objects.hash(Name);
     }
 }
+
--- src/main/java/faculdade/Main.java
+++ src/main/java/faculdade/Main.java
@@ -1,69 +1,85 @@
+import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.lang.reflect.Type;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.Scanner;
 import java.util.Set;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
 import Books.Book;
-import UserIO.BooksIO;
 
 public class Main {
+    static Scanner scan = new Scanner(System.in);
+
+    private static BufferedReader FileReader() throws IOException {
+        return new BufferedReader(new FileReader("books.json"));
+    }
+    static Map<Integer, String> interestsMap = Map.of(
+        1, "Senhor dos Pastéis",
+        2, "Jogos Ferozes",
+        3, "Mistborn"
+    );
+
+    public static String askInterests(){
+        System.out.println("wat are your interests?");
+        System.out.println("1. Senhor dos Pastéis");
+        System.out.println("2. Jogos Ferozes");
+        System.out.println("3. Mistborn");
+
+        return interestsMap.get(scan.nextInt());
+    }
+
     public static void main(String[] args) {
         Gson gson = new Gson();
         Map<String, Map<String, String>> fromJson;
         Type type = new TypeToken<Map<String, Map<String, String>>>(){}.getType();
+
         Graph graphBook = new Graph();
+        String interests = askInterests();
+        Set<Book> recomendations = null; // i shouldn't use null as default value :\
 
-        String interests = BooksIO.askInterests();
-
-        // i've no idea what happened here
-        try (FileReader json = new FileReader("books.json")) {
+        try (BufferedReader json = FileReader()) {
             fromJson = gson.fromJson(json, type);
-            BookReference bookRef = new BookReference();
+            // recomendations = bookRef.bookRecomendations(fromJson, interests);
+            recomendations = new BookReference().bookRecomendations(fromJson, interests);
 
-            Set<Book> recomendations = bookRef.bookRecomendations(fromJson, interests);
-            Set<Book> recomendationsSnapshot = new HashSet<>(recomendations);
-            for (Book BookSet : recomendationsSnapshot) {
-                recomendations.remove(BookSet);
-                graphBook.newGraph(BookSet, recomendations);
-                recomendations.addAll(recomendationsSnapshot);
-            }
-            for (Map.Entry<Book, Set<Book>> b : graphBook.hashmap.entrySet()) {
-                System.out.println(b + "\n");
-                //last book:
-                // System.out.println(bookRef.book.Name);
-            }
+        } catch (Exception e) {
+            e.getMessage();
+            System.exit(1);
+        }
 
-            for (Book book : recomendations) {
-                System.out.println("-".repeat(16));
-                System.out.println("Coleção: " + book.Set);
-                System.out.println(book.getID());
-                System.out.println(book.Name);
-                System.out.println(book.Author);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
+        Set<Book> recomendationsSnapshot = new HashSet<>(recomendations);
+        for (Book BookSet : recomendationsSnapshot) {
+            recomendations.remove(BookSet);
+            graphBook.putInGraph(BookSet, recomendations);
+            recomendations.addAll(recomendationsSnapshot);
+        }
+        for (Map.Entry<Book, Set<Book>> b : graphBook.hashmap.entrySet()) {
+            System.out.println(b);
         }
     }
 }
 
 class BookReference {
-    Book book;
     Set<Book> recomendations = new HashSet<>();
+    Map<String, String> inner;
+    Book book;
 
     public Set<Book> bookRecomendations(Map<String, Map<String, String>> fromJson, String interests){
         for (Map.Entry<String, Map<String, String>> entry : fromJson.entrySet()) {
-            Map<String, String> inner = entry.getValue();
+            inner = entry.getValue();
+            book = new Book(
+                inner.get("Name"),
+                inner.get("Genre"),
+                inner.get("Author"),
+                inner.get("Set")
+            );
             if (inner.get("Set").equals(interests)){
-                book = new Book(inner.get("Name"),
-                    inner.get("Genre"),
-                    inner.get("Author"),
-                    inner.get("Set"));
                 recomendations.add(book);
             }
         }
@@ -73,10 +89,8 @@ class BookReference {
 
 class Graph{
     HashMap<Book, Set<Book>> hashmap = new HashMap<>();
-    public HashMap<Book, Set<Book>> newGraph(Book book, Set<Book> recomendations){
-        hashmap.put(book, recomendations);
-        // System.out.println(hashmap + "\n");
+    public HashMap<Book, Set<Book>> putInGraph(Book book, Set<Book> recomendations){
+        hashmap.put(book, new HashSet<>(recomendations));
         return hashmap;
     }
 }
-
